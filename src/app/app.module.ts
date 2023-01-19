4c9cf9425fca66b470fd1488e0268a3d3fc62540
vinodgithubu <attatvinod@gmail.com>
2023-01-19 19:35:07 +0530

    Adding material UI lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './core/components/list/list.component';
+import { RecordComponent } from './core/components/record/record.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  exports: [
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
+})
+export class MaterialModules {}
+@NgModule({
+  declarations: [AppComponent, ListComponent, RecordComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModules,
+    MatDialogModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
